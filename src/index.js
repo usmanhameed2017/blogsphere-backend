580ec45eae5f05862848f071e52e15e3ae757b1f
Usman Hameed <usmanhameed1790@gmail.com>
2025-05-19 17:48:00 +0500

    Configure database connection

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 require("dotenv").config();
 const app = require("./app");
+const { port } = require("./constants");
+const connectDB = require("./database/connection");
+
+// Database connection
+connectDB()
+.then(() => {
+    app.on("error", () => console.log("Failed to run application"));
+    app.listen(port, () => console.log(`Server is started and running at http://localhost:${port}`));
+}).catch(error => console.log(error.message));
