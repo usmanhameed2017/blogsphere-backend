ec01a291120cf538f07b858e840cb92ca0d60b57
Usman Hameed <usmanhameed1790@gmail.com>
2025-05-20 14:45:22 +0500

    Validation added to signup controller

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -4,10 +4,22 @@ const ApiResponse = require("../utils/ApiResponse");
 
 // User signup
 const signup = async (request, response) => {
+    const { email, username, password, cpassword } = request.body;
+
+    // Identical passwords
+    if(password !== cpassword) throw new ApiError(400, "Password & confirm password must be identical");
+
+    // If email or username exist
+    const user = await User.isUserExist(email, username);
+    if(user) throw new ApiError(400, "The email or username you entered is already exist!");
+
+    // Create user
     try 
     {
         const user = await User.create(request.body);
-        return response.status(201).json(new ApiResponse(201, user, "Account has been created successfully"));
+        const userData = user.toObject();
+        delete userData.password; // Exclude password
+        return response.status(201).json(new ApiResponse(201, userData, "Account has been created successfully"));
     }
     catch (error) 
     {
