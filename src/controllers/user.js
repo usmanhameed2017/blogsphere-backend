d5a62ea054efb2ca1aa8e9cf77481e9332d12c9c
Usman Hameed <usmanhameed1790@gmail.com>
2025-05-20 13:22:28 +0500

    Api Error class added

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,15 +1,17 @@
 const User = require("../models/user");
+const ApiError = require("../utils/ApiError");
+const ApiResponse = require("../utils/ApiResponse");
 
 // User signup
 const signup = async (request, response) => {
     try 
     {
         const user = await User.create(request.body);
-        return response.status(201).json({ data:user, message:"Account has been created successfully", success:true });
+        return response.status(201).json(new ApiResponse(201, user, "Account has been created successfully"));
     }
     catch (error) 
     {
-        return response.status(500).json({ message:error.message, success:false });
+        throw new ApiError(500, error.message);
     }
 };
 
