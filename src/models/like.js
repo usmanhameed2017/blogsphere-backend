538c8be3ce22ccf5746d616795e15e031f66c2e5
Usman Hameed <usmanhameed1790@gmail.com>
2025-05-22 15:11:01 +0500

    Setup like schema model

---
+++ src/models/like.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require("mongoose");
+
+// Schema
+const likeSchema = new Schema({
+    blog_id:{
+        type: Schema.Types.ObjectId,
+        ref:"Blog",
+        trim:true
+    },
+    likedBy:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+        trim:true
+    }
+});
+
+// Model
+const Like = model("Like", likeSchema);
+
+module.exports = Like;
