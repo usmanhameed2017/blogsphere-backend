4d8fd9ed317a0b6bc5a78381a8da38d1330e05be
Usman Hameed <usmanhameed1790@gmail.com>
2025-05-22 18:42:50 +0500

    Setup comment schema model

---
+++ src/models/comment.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require("mongoose");
+
+// Schema
+const commentSchema = new Schema({
+    text:{
+        type:String,
+        trim:true,
+        required:true
+    },
+    blogID:{
+        type:Schema.Types.ObjectId,
+        ref:"Blog",
+        trim:true
+    },
+    commentedBy:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+        trim:true
+    }
+});
+
+// Model
+const Comment = model("Comment", commentSchema);
+
+module.exports = Comment;
